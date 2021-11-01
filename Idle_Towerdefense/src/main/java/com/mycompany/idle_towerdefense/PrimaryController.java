0b7b763a9d1eef1b09c26d332377bd0097bb478e
Rawlpluks <claramajse@gmail.com>
2021-11-01 12:28:44 +0100

    start

---
+++ Idle_Towerdefense/src/main/java/com/mycompany/idle_towerdefense/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.mycompany.idle_towerdefense;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ Idle_Towerdefense/src/main/java/com/mycompany/idle_towerdefense/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.mycompany.idle_towerdefense;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
