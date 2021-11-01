f14b90cad4bd33f4f3fd3181e46478c63ab46d29
Drulle19 <danielschmidtlarsen19@gmail.com>
2021-11-01 13:35:31 +0100

    #4 Database sat op og kan hente data (ikke uploade)

--- Idle_Towerdefense/src/main/java/com/mycompany/idle_towerdefense/Database.java
+++ Idle_Towerdefense/src/main/java/com/mycompany/idle_towerdefense/Database.java
@@ -5,10 +5,53 @@
  */
 package com.mycompany.idle_towerdefense;
 
+import java.util.ArrayList;
+import java.sql.*;
+
 /**
  *
  * @author danie
  */
 public class Database {
+    private final String connectionString = "jdbc:sqlite:src/Database.db";
+   
+   public ArrayList<Highscore> getAllHighscores() throws SQLException, Exception {
+        
+        ArrayList<Highscore> allHighscores = new ArrayList<>();
+        
+        Connection conn = null;
+        Class.forName("org.sqlite.JDBC");
+        
+        //Skab forbindelse til databasen...
+        
+        try {          
+          conn = DriverManager.getConnection(connectionString);
+        } 
+        catch ( SQLException e ) {
+          //Skrive fejlhåndtering her
+          System.out.println("DB Error: " + e.getMessage());
+        }
+        
+        //Hent alle personer fra databasen v.h.a. SQL
+        try{ 
+            Statement stat = conn.createStatement();   
+
+            //Læser fra database alt data fra databasetabellen people.   
+            ResultSet rs = stat.executeQuery("User, Score");
+
+            //Løber data igennem via en løkke og skriver det up.    
+            while (rs.next()) {
+                allHighscores.add(new Highscore(rs.getString("User"), rs.getInt("Score")));
+            }
+            rs.close();
+        }
+        catch ( SQLException e ) {
+            //Skrive fejlhåndtering her
+            System.out.println("DB Error: " + e.getMessage());
+        }
+        //Luk forbindelsen til databasen
+        conn.close();
     
+        return allHighscores;
+    }
 }
--- Idle_Towerdefense/src/main/java/module-info.java
+++ Idle_Towerdefense/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.idle_towerdefense {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.mycompany.idle_towerdefense to javafx.fxml;
     exports com.mycompany.idle_towerdefense;
