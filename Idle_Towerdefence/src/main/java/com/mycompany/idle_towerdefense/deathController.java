7b9630f3af263b8d74da5929b04dc78fc3f39825
Drulle19 <danielschmidtlarsen19@gmail.com>
2021-12-01 08:45:55 +0100

    #2 laver screen

--- Idle_Towerdefence/src/main/java/com/mycompany/idle_towerdefense/deathController.java
+++ Idle_Towerdefence/src/main/java/com/mycompany/idle_towerdefense/deathController.java
@@ -6,10 +6,25 @@
 
 package com.mycompany.idle_towerdefense;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 /**
  *
  * @author Clara Maj
  */
-public class deathController {
-
+public class deathController implements Initializable{
+    
+    @FXML
+    TableView table;
+    TableColumn name;
+    TableColumn points;
+    
+    public void initialize(URL url, ResourceBundle rb){
+        
+    }
 }
